5d1f7462ed9bcc3621d77ebd483d3d80b84f68c9
omniapropstate-dot <omniaproprealestate@gmail.com>
2026-04-14 21:32:47 -0400

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+const SUPABASE_URL = 'https://kqvhuzwepewqjlffmqvo.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtxdmh1endlcGV3cWpsZmZtcXZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2Nzg5NjUsImV4cCI6MjA5MTI1NDk2NX0.DPlSSi4seRnPm43G_b4IhVn2Has1m3AiprR1bLFLnHw'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
